543e9fa231a30853b287e040f4bf8bb5a1107d43
durosyan <durosyan@outlook.com>
2020-10-29 00:43:58 +0000

    added packages and config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,57 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
+/* https://www.gatsbyjs.com/docs/gatsby-config/ */
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  siteMetadata: {
+    title: "weka projects",
+    siteUrl: "https://www.wekaprojects.co.nz",
+    description: "online store for weka projects",
+  },
+  plugins: [
+    "gatsby-theme-material-ui",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-layout",
+      options: {
+        component: require.resolve("./src/components/Layout.js"),
+      }
+    },
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Weka Projects",
+        short_name: "Weka",
+        icon: "src/assets/logo-feathers.svg",
+        start_url: "/",
+        background_color: "#262626",
+        theme_color: "#e5e5e5",
+        display: "standalone",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/
+        }
+      }
+    },
+    {
+      resolve: "gatsby-theme-material-ui",
+      options: {
+        webFontsConfig: {
+          fonts: {
+            google: [
+              {
+                family: "Amatic SC",
+                variants: ["400", "700"],
+              },
+              {
+                family: "Permanent Marker",
+                variants: ["400"],
+              }
+            ],
+          },
+        },
+      },
+    },
+  ],
+};
